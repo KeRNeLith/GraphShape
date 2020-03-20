992d46eb04d3edfda23e77e68de5ae30fdbaf9fb
Alexandre Rabérin <kernelith@live.fr>
2020-03-21 00:27:49 +0100

    Get rid of props for package version.

--- src/GraphShape/Properties/AssemblyInfo.cs
+++ src/GraphShape/Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("GraphShape.Tests")]
+[assembly:InternalsVisibleTo("GraphShape.Controls")]
+[assembly:InternalsVisibleTo("GraphShape.Sample")]
