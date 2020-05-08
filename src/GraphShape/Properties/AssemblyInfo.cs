dad2e3e7202fc800f045a44ebc9f621818941039
Alexandre Rabérin <kernelith@live.fr>
2020-05-08 11:36:48 +0200

    Add test project for GrapheShape.Controls.

--- src/GraphShape/Properties/AssemblyInfo.cs
+++ src/GraphShape/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("GraphShape.Tests")]
 [assembly:InternalsVisibleTo("GraphShape.Controls")]
+[assembly:InternalsVisibleTo("GraphShape.Controls.Tests")]
 [assembly:InternalsVisibleTo("GraphShape.Sample")]
