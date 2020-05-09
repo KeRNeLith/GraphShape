185cf6972c5a68f3c29f3f0c6606ec4aa01117c7
Alexandre Rabérin <kernelith@live.fr>
2020-05-09 17:59:48 +0200

    Exclude unwanted files from coverage.
    Add missing tests.

---
+++ tests/GraphShape.Controls.Tests/DisposableHelpersTests.cs
@@ -0,0 +1,25 @@
+﻿using GraphShape.Controls.Utils;
+using NUnit.Framework;
+
+namespace GraphShape.Controls.Tests
+{
+    /// <summary>
+    /// Tests for <see cref="DisposableHelpers"/>.
+    /// </summary>
+    [TestFixture]
+    internal class DisposableHelpersTests
+    {
+        [Test]
+        public void Finally()
+        {
+            Assert.IsNotNull(DisposableHelpers.Finally(() => {}));
+
+            int counter = 0;
+            using (DisposableHelpers.Finally(() => ++counter))
+            {
+                Assert.AreEqual(0, counter);
+            }
+            Assert.AreEqual(1, counter);
+        }
+    }
+}
