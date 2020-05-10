f521c2ff81f66a4ec2e06f51397b9938b193382a
Alexandre Rabérin <kernelith@live.fr>
2020-05-10 18:10:03 +0200

    Make JetBrains annotations a shared file.

--- src/GraphShape/Properties/AssemblyInfo.cs
+++ src/GraphShape/Properties/AssemblyInfo.cs
@@ -1,7 +1,3 @@
 using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("GraphShape.Tests")]
-[assembly:InternalsVisibleTo("GraphShape.Controls")]
-[assembly:InternalsVisibleTo("GraphShape.Controls.Tests")]
-[assembly:InternalsVisibleTo("GraphShape.Sample")]
-[assembly:InternalsVisibleTo("GraphShape.Optimization")]
